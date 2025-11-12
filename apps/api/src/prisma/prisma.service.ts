48fa56b8c33c81a1205d43a82439a3ba2dab8abb
agear13 <76278469+agear13@users.noreply.github.com>
2025-11-12 10:44:22 +0800

    fix(api): replace require with ES import for PrismaService

--- apps/api/src/prisma/prisma.service.ts
+++ apps/api/src/prisma/prisma.service.ts
@@ -1,23 +1,13 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-
-// Use require to avoid TS/module interop issues with @prisma/client in CI
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { PrismaClient } = require('@prisma/client');
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   async onModuleInit() {
-    if (typeof this.$connect === 'function') {
-      await this.$connect();
-    }
+    await this.$connect();
   }
 
   async onModuleDestroy() {
-    if (typeof this.$disconnect === 'function') {
-      await this.$disconnect();
-    }
+    await this.$disconnect();
   }
 }
